49d19b009d35cb2868ef40eb6db3b53015bc32fd
johnstoermer <john.stoermer@gmail.com>
2018-03-13 15:31:55 -0400

    Add files via upload

---
+++ Assets/Scripts/DungeonGame.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGame : MonoBehaviour {
+    private int redScore = 0;
+    private int blueScore = 0;
+    private int greenScore = 0;
+    public int playerMaxHealth = 100;
+    public int playerHealth = 100;
+    public int playerMagicResist = 0;
+    public int playerPhysicalResist = 0;
+    public int enemyMaxHealth = 100;
+    public int enemyHealth = 100;
+    public int enemyMagicResist = 0;
+    public int enemyPhysicalResist = 0;
+    public int enemyMagicDamage = 0;
+    public int enemyPhysicalDamage = 0;
+    public int enemyAttackRate = 1;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+	}
+
+    void playGame() {
+
+    }
+
+    void fight() {
+        while (enemyHealth > 0) {
+            StartCoroutine(damagePlayer());
+        }
+    }
+
+    IEnumerator damagePlayer() {
+        playerHealth -= enemyMagicDamage / (1 + playerMagicResist) + enemyPhysicalDamage / (1 + playerPhysicalResist);
+        yield return new WaitForSeconds(enemyAttackRate);
+    }
+
+    void damageEnemy(int magicDamage, int physicalDamage) {
+        enemyHealth -= magicDamage / (1 + enemyMagicResist) + physicalDamage / (1 + enemyPhysicalResist);
+    }
+
+    void healPlayer(int healing) {
+        playerHealth += healing;
+        if (playerHealth >= playerMaxHealth) {
+            playerHealth = playerMaxHealth;
+        }
+    }
+
+    public void updateScore(int redScore, int blueScore, int greenScore) {
+        damageEnemy(redScore, blueScore);
+        healPlayer(greenScore);
+    }
+}
---
+++ Assets/Scripts/ShapeSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSpawner : MonoBehaviour {
+    public Color[] colors;
+    public Color[] nextColors;
+    int colorIndex = 0;
+    int nextColorIndex = 0;
+
+    public GameObject[] shapes;
+    public GameObject[] nextShapes;
+
+    GameObject upNextObject = null;
+
+    int shapeIndex = 0;
+    int nextShapeIndex = 0;
+
+    public void SpawnShape() {
+
+        shapeIndex = nextShapeIndex;
+        colorIndex = nextColorIndex;
+
+        GameObject shape = Instantiate(shapes[shapeIndex],
+            transform.position,
+            Quaternion.identity);
+
+        foreach (Transform block in shape.transform) {
+            block.GetComponent<SpriteRenderer>().color = colors[colorIndex];
+        }
+
+        nextShapeIndex = Random.Range(0, 6);
+        nextColorIndex = Random.Range(0, 3);
+
+        Vector3 nextShapePos = new Vector3(-20f, 18f, 0);
+
+        if (upNextObject != null)
+            Destroy(upNextObject);
+
+        upNextObject = Instantiate(nextShapes[nextShapeIndex],
+            nextShapePos,
+            Quaternion.identity);
+
+        foreach (Transform block in upNextObject.transform) {
+            block.GetComponent<SpriteRenderer>().color = colors[nextColorIndex];
+        }
+    }
+
+    void Start() {
+        nextShapeIndex = Random.Range(0, 6);
+
+        SpawnShape();
+
+    }
+
+    void Update() {
+
+    }
+}
